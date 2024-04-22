8f7d0ca35b77ac176361b8f2caf194daf31e20e5
GnowEkalb <143014815+GnowEkalb@users.noreply.github.com>
2024-04-22 10:21:06 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,30 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+  private int minSteps, totalSteps;
+  private int activeDays, totalDays;
+  
+  public StepTracker(int min){
+    minSteps = min;
+    totalSteps = 0;
+    activeDays = 0;
+    totalDays = 0;
+  }
+  
+  public void addDailySteps(int steps){
+    totalSteps+=steps;
+    totalDays++;
+    if (steps >= minSteps)
+      activeDays++;
+  }
+  
+  public int activeDays(){
+    return activeDays;
+  }
+  
+  public double averageSteps(){
+    if (totalDays == 0)
+      return 0;
+    else return (double)totalSteps/totalDays;
+  }
 } 
